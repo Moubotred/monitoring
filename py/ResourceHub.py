437b5b855a25c791f40bc2556637492307d1a27b
Moubotred <fakesarnamer@gmail.com>
2024-10-20 18:39:59 -0500

    implemetacion de funcion de hasber api

--- py/ResourceHub.py
+++ py/ResourceHub.py
@@ -156,6 +156,63 @@ def ConsultaApiLds(suministro):
         os.makedirs(imagenes)
         ConsultaApiLds(suministro)
 
+def Hasber_Sistema_Envios(suministro):
+    # URL a la que se va a hacer la solicitud POST
+    url = "https://hasbercourier.easyenvios.com/modulos/controlador/herramientas/con_cargosenvios.php?tabla=congrid_envio_seguimiento"
+
+    # Encabezados
+    headers = {
+        "Host": "hasbercourier.easyenvios.com",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:91.0) Gecko/20100101 Firefox/91.0",
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "X-Requested-With": "XMLHttpRequest",
+        "Origin": "https://hasbercourier.easyenvios.com",
+        "Referer": "https://hasbercourier.easyenvios.com/modulos/seguimiento.php",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "Te": "trailers",
+        "Connection": "keep-alive"
+    }
+
+    # Parámetros del cuerpo de la solicitud (POST data)
+    data = {
+        "ciacodigo": "17",
+        "succodigo": "001",
+        "asenombre": "",
+        "codigoenvio": "",
+        "codigounico": f"{suministro}",
+        "asepropuesta": "",
+        "strnrodocumento": "",
+        "page": "1",
+        "rows": "10",
+        "sort": "a.asenumero",
+        "order": "asc"
+    }
+
+    # Hacer la solicitud POST
+    response = requests.post(url, headers=headers, data=data)
+
+    # Verificar el estado de la respuesta de estado
+    if response.status_code == 200:
+        # pasar datos a json
+        data = response.json()
+        try:
+            # buscar clave de diccionario
+            if data['rows'][0]['artnombre'] == 'CARTAS / REEMPLAZO DE MEDIDOR EMPRESAS':
+                url_file = data['rows'][0]['imagen1']
+                FileWebDownloads(url_file,suministro)
+                ConvertPdf(suministro)
+                Templades(f"{suministro}.tif")
+                return suministro                    
+        except IndexError:
+            return False
+    else:
+        return False
+
 def Directorios(directorio):
     def Servidor(func):
         def python(*args,**kwargs):
