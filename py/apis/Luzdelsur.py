ed9a54bf1235f331d9f37668c3b9feadebb2e8b4
Moubotred <fakesarnamer@gmail.com>
2024-12-17 19:49:09 -0500

    control de execpcion para solicitudes

--- py/apis/Luzdelsur.py
+++ py/apis/Luzdelsur.py
@@ -32,35 +32,40 @@ async def LuzdelsurRecibo(suministro):
     }
 
     # Realiza la solicitud POST de manera asíncrona
-    async with httpx.AsyncClient() as client:
+    async with httpx.AsyncClient(timeout=60) as client:
         response = await client.post(url, json=payload, headers=headers)
 
-    # Verifica si la solicitud fue exitosa
-    if response.status_code == 200:
-        # Convertir la respuesta a JSON
-        data = response.json()
-        
-        imagen_base64 = data['datos']['archivoBase64']
-
-        if not imagen_base64:
-            return '⚒️ el recibo no existe en en la web 🥷'
-        
-        # Decodificar y guardar la imagen en un archivo de manera asíncrona
         try:
-            descargas = pydirecion.descargas
-            home = os.path.expanduser('~/')
-            directorio = os.path.abspath(os.path.join(home,descargas))
-            archivo = f'{suministro}.png'
-            async with aiofiles.open(archivo, "wb") as img_file:
-                await img_file.write(base64.b64decode(imagen_base64))
-                # await anime(suministro)
-                await lolis(suministro)
-                await Directorio(archivo,directorio)
+
+            # Verifica si la solicitud fue exitosa
+            if response.status_code == 200:
+                # Convertir la respuesta a JSON
+                data = response.json()
                 
-            return archivo
-        
-        except TypeError:
-            return "La API no devolvió una imagen válida en formato base64"
-        
-    elif response.status_code != 200:
-        return '⚠️ la web actualizo el codigo informar inmediatamente al adminstrador del Bot Miharu ☣️'
+                imagen_base64 = data['datos']['archivoBase64']
+
+                if not imagen_base64:
+                    return '⚒️ el recibo no existe en en la web 🥷'
+                
+                # Decodificar y guardar la imagen en un archivo de manera asíncrona
+                try:
+                    descargas = pydirecion.descargas
+                    home = os.path.expanduser('~/')
+                    directorio = os.path.abspath(os.path.join(home,descargas))
+                    archivo = f'{suministro}.png'
+                    async with aiofiles.open(archivo, "wb") as img_file:
+                        await img_file.write(base64.b64decode(imagen_base64))
+                        # await anime(suministro)
+                        await lolis(suministro)
+                        await Directorio(archivo,directorio)
+                        
+                    return archivo
+                
+                except TypeError:
+                    return "La API no devolvió una imagen válida en formato base64"
+                
+            elif response.status_code != 200:
+                return '⚠️ la web actualizo el codigo informar inmediatamente al adminstrador del Bot Miharu ☣️'
+            
+        except httpx.ReadTimeout:
+            return '💤 tiempo de solicitud agotada ⏳'
