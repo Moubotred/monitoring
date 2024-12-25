7193cc70ad9fd88dec438309d3269768a3de6d37
Moubotred <fakesarnamer@gmail.com>
2024-12-25 15:10:27 -0500

    commit de prueba

--- py/constantes.py
+++ py/constantes.py
@@ -1,4 +1,6 @@
+
 # uwu
+
 class pydirecion:
     # no ponder home ya que los scripta ya cargan el home lo la ruta de donde de descargara el conytenido
     descargas = 'Proyects/monitoring/descargas'
